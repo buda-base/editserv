a9a77df2440c1026fdd0afe0af15ef288e3cfe94
Marc Agate <agate.marc@gmail.com>
2019-06-25 11:27:26 -0400

    finally got a working spring boot test environment

--- src/main/test/editservio/bdrc/edit/test/BasicTest.java
+++ src/main/test/editservio/bdrc/edit/test/BasicTest.java
@@ -5,35 +5,39 @@ import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.entity.StringEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import io.bdrc.edit.EditApplication;
 import io.bdrc.edit.EditConfig;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = EditApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class BasicTest {
+
+    @Autowired
+    Environment environment;
 
     @BeforeClass
     public static void init() {
         EditConfig.init();
+
     }
 
+    @Test
     public void sendPutTaskIdRequest() throws ClientProtocolException, IOException {
         HttpClient client = HttpClientBuilder.create().build();
-        HttpPut put = new HttpPut("http://localhost:8080/tasks");
-        String json = "  {\n" + "    \"id\": \"AAAAAA\",\n" + "    \"saveMsg\": \"Test save message\",\n" + "    \"shortName\": \"Yoga Collection\",\n" + "    \"message\":\"about the task\",\n" + "    \"user\":\"marc\", \n"
-                + "    \"patch\":\"here is the latest version of the content of the patch AAAA\" \n" + "    \n" + "  } ";
-        StringEntity entity = new StringEntity(json);
-        put.setEntity(entity);
-        put.setHeader("Accept", "application/json");
-        put.setHeader("Content-type", "application/json");
-        HttpResponse response = client.execute(put);
+        HttpGet get = new HttpGet("http://localhost:" + environment.getProperty("local.server.port") + "/ping");
+        HttpResponse response = client.execute(get);
         System.out.println(response);
-        assert (response.getStatusLine().getStatusCode() == 204);
+        assert (response.getStatusLine().getStatusCode() == 200);
     }
 
 }

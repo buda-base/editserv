ec0bd83006f0cc0ff6fe7190be486cc2658f013a
Marc Agate <agate.marc@gmail.com>
2020-07-29 10:58:18 -0400

    cleanup + full set of tests

--- src/test/java/io/bdrc/edit/test/TestModelUtils.java
+++ src/test/java/io/bdrc/edit/test/TestModelUtils.java
@@ -1,12 +1,9 @@
 package io.bdrc.edit.test;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
@@ -17,20 +14,16 @@ import org.apache.jena.ontology.OntModelSpec;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.ResourceFactory;
-import org.apache.jena.rdf.model.SimpleSelector;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.reasoner.Reasoner;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import io.bdrc.edit.EditConfig;
-import io.bdrc.edit.EditConstants;
 import io.bdrc.edit.commons.data.OntologyData;
 import io.bdrc.edit.commons.ops.CommonsValidate;
-import io.bdrc.edit.helpers.ModelUtils;
 import io.bdrc.edit.txn.exceptions.NotModifiableException;
 import io.bdrc.edit.txn.exceptions.UnknownBdrcResourceException;
 
@@ -58,6 +51,10 @@ public class TestModelUtils {
 
     static Model ontmodel = null;
     static Reasoner bdrcReasoner = null;
+    static Model initial = ModelFactory.createDefaultModel();
+    static Model missingSon = ModelFactory.createDefaultModel();
+    static Model moreSon = ModelFactory.createDefaultModel();
+    static Model edited = ModelFactory.createDefaultModel();
 
     @BeforeClass
     public static void init() throws Exception {
@@ -74,69 +71,54 @@ public class TestModelUtils {
         ontSpec.setDocumentManager(ontManager);
         ontmodel = ontManager.getOntology("http://purl.bdrc.io/ontology/admin/", ontSpec);
         bdrcReasoner = BDRCReasoner.getReasoner(ontmodel, owlSchemaBase + "reasoning/kinship.rules", true);
-    }
-
-    // @Test
-    public void checkRemovedTriples() throws IOException {
-        Model initial = ModelFactory.createDefaultModel();
-        Model edited = ModelFactory.createDefaultModel();
-        InputStream in = TestModelUtils.class.getClassLoader().getResourceAsStream("P1583.ttl");
-        initial.read(in, null, "TTL");
-        in = TestModelUtils.class.getClassLoader().getResourceAsStream("P1583_missingProps.ttl");
-        edited.read(in, null, "TTL");
-        in.close();
-        Set<Statement> removed = ModelUtils.ModelComplementAsSet(initial, edited);
-        ArrayList<String> uris = new ArrayList<>();
-        for (Statement st : removed) {
-            uris.add(st.getObject().asResource().getURI());
-        }
-        for (Resource r : missingObjects) {
-            assert (uris.contains(r.getURI()));
-        }
-    }
-
-    public void displayInverseSymetric() {
-        Model initial = ModelFactory.createDefaultModel();
         InputStream in = TestModelUtils.class.getClassLoader().getResourceAsStream("P705.ttl");
         initial.read(in, null, "TTL");
-        List<Statement> inverses = CommonsValidate.getNeighboursFromInverse(initial.listStatements().toSet());
-        List<Statement> symetrics = CommonsValidate.getNeighboursFromSymmetric(initial.listStatements().toSet());
-        System.out.println("Neighbours Inverse >>" + inverses);
-        System.out.println("Neighbours Symmetric >>" + symetrics);
+        in = TestModelUtils.class.getClassLoader().getResourceAsStream("P705_missingSon.ttl");
+        missingSon.read(in, null, "TTL");
+        in = TestModelUtils.class.getClassLoader().getResourceAsStream("P705_oneMoreSon.ttl");
+        moreSon.read(in, null, "TTL");
+        in = TestModelUtils.class.getClassLoader().getResourceAsStream("P705_edited.ttl");
+        edited.read(in, null, "TTL");
     }
 
     @Test
-    public void findInverseTripleFromModels() throws IOException, UnknownBdrcResourceException, NotModifiableException {
-        Model initial = ModelFactory.createDefaultModel();
-        Model edited = ModelFactory.createDefaultModel();
-        // This is the graph editor (i.e the model obtained from local git then cleaned
-        // up for edition)
-        InputStream in = TestModelUtils.class.getClassLoader().getResourceAsStream("P705.ttl");
-        initial.read(in, null, "TTL");
-        // This is the same graph editor as above without one triple
-        in = TestModelUtils.class.getClassLoader().getResourceAsStream("P705_missingSon.ttl");
-        edited.read(in, null, "TTL");
-        in.close();
+    public void findAllTriplesToProcessFromModels() throws IOException, UnknownBdrcResourceException, NotModifiableException {
+        findTriplesToProcess(initial, edited);
+    }
+
+    @Test
+    public void findTriplesToRemoveFromModels() throws IOException, UnknownBdrcResourceException, NotModifiableException {
+        findTriplesToProcess(initial, missingSon);
+    }
+
+    @Test
+    public void findTriplesToAddFromModels() throws IOException, UnknownBdrcResourceException, NotModifiableException {
+        findTriplesToProcess(initial, moreSon);
+    }
+
+    public static void findTriplesToProcess(Model init, Model edit) throws IOException, UnknownBdrcResourceException, NotModifiableException {
         System.out.println("we apply the reasoner on non-updated father and get:");
-        Model oldInferredModel = ModelFactory.createInfModel(bdrcReasoner, initial).getDeductionsModel();
+        Model oldInferredModel = ModelFactory.createInfModel(bdrcReasoner, init).getDeductionsModel();
         oldInferredModel.remove(BDRCReasoner.deReasonToRemove(ontmodel, oldInferredModel));
         System.out.println(oldInferredModel.listStatements().toList());
         System.out.println("we apply the reasoner to updated father and get:");
-        Model newInferredModel = ModelFactory.createInfModel(bdrcReasoner, edited).getDeductionsModel();
+        Model newInferredModel = ModelFactory.createInfModel(bdrcReasoner, edit).getDeductionsModel();
         System.out.println(newInferredModel.listStatements().toList());
         System.out.println("we simplify the inferred triples to clean things up a bit and obtain:");
         newInferredModel.remove(BDRCReasoner.deReasonToRemove(ontmodel, newInferredModel));
-        // System.out.println("After cleanup we obtain:");
-        // System.out.println(newInferredModel.listStatements().toList());
-        // we use the full newInferred instead of the cleanup one
         Model triplesToRemove = oldInferredModel.difference(newInferredModel);
         Model triplesToAdd = newInferredModel.difference(oldInferredModel);
         System.out.println("-------------------------------------------");
         System.out.println("These were removed by the editor:");
-        Set<Statement> diff = CommonsValidate.getDiffRemovedTriples(initial, edited);
+        Set<Statement> diff = CommonsValidate.getDiffRemovedTriples(init, edit);
         System.out.println("-------------------------------------------");
         for (Statement d : diff) {
             System.out.println("triple removed by edition -->: " + d);
+        }
+        Set<Statement> adds = CommonsValidate.getDiffAddedTriples(init, edit);
+        System.out.println("-------------------------------------------");
+        for (Statement ad : adds) {
+            System.out.println("triple added by edition -->: " + ad);
         }
         System.out.println("-------------------------------------------");
         System.out.println("we should add the following triples to the son model:");
@@ -157,80 +139,6 @@ public class TestModelUtils {
             }
         }
         System.out.println("-------------------------------------------");
-    }
-
-    // @Test
-    public void findInverseTriple() throws IOException, UnknownBdrcResourceException, NotModifiableException {
-        Model fatherM = ModelFactory.createDefaultModel();
-        Model sonM = ModelFactory.createDefaultModel();
-        fatherM.add(fatherR, fatherM.createProperty(BDO, "personGender"), fatherM.createResource(BDR + "GenderMale"));
-        sonM.add(sonR, sonM.createProperty(BDO, "personGender"), sonM.createResource(BDR + "GenderMale"));
-        System.out.println("we have the father:");
-        System.out.println(fatherM.listStatements().toList());
-        System.out.println("and the son:");
-        System.out.println(sonM.listStatements().toList());
-        System.out.println("we receive an update that makes the father look like:");
-        Model fatherUpdateM = ModelFactory.createDefaultModel().add(fatherM);
-        fatherUpdateM.add(fatherR, hasSon, sonR);
-        System.out.println(fatherUpdateM.listStatements().toList());
-        System.out.println("we apply the reasoner on non-updated father and get:");
-        Model oldInferredModel = ModelFactory.createInfModel(bdrcReasoner, fatherM).getDeductionsModel();
-        System.out.println(oldInferredModel.listStatements().toList());
-        System.out.println("we apply the reasoner to updated father and get:");
-        Model newInferredModel = ModelFactory.createInfModel(bdrcReasoner, fatherUpdateM).getDeductionsModel();
-        System.out.println(newInferredModel.listStatements().toList());
-        System.out.println("we simply the inferred triples to clean things up a bit and obtain:");
-        newInferredModel.remove(BDRCReasoner.deReasonToRemove(ontmodel, newInferredModel));
-        System.out.println(newInferredModel.listStatements().toList());
-        System.out.println("then we diff that with the original inferred model and select the triples related to the son.");
-        Model triplesToRemove = oldInferredModel.difference(newInferredModel);
-        Model triplesToAdd = newInferredModel.difference(oldInferredModel);
-        SimpleSelector ssSon = new SimpleSelector(sonR, null, (RDFNode) null);
-        System.out.println("we should add the following triples to the son model:");
-        System.out.println(triplesToAdd.listStatements(ssSon).toList());
-        System.out.println("we should remove the following triples from the son model:");
-        System.out.println(triplesToRemove.listStatements(ssSon).toList());
-    }
-
-    @Test
-    public void findTriplesToRemove() throws IOException, UnknownBdrcResourceException, NotModifiableException {
-        System.out.println(
-                System.lineSeparator() + System.lineSeparator() + "<--------Now USING ONTYOLOGY INFERENCE ------->" + System.lineSeparator());
-        Model initial = ModelFactory.createDefaultModel();
-        Model edited = ModelFactory.createDefaultModel();
-        // This is the graph editor (i.e the model obtained from local git then cleaned
-        // up for edition)
-        InputStream in = TestModelUtils.class.getClassLoader().getResourceAsStream("P705.ttl");
-        initial.read(in, null, "TTL");
-        // This is the same graph editor as above without one triple
-        in = TestModelUtils.class.getClassLoader().getResourceAsStream("P705_missingSon.ttl");
-        edited.read(in, null, "TTL");
-        in.close();
-        Set<Statement> removed = CommonsValidate.getDiffRemovedTriples(initial, edited);
-        System.out.println("These were removed by the editor:");
-        System.out.println("-------------------------------------------");
-        for (Statement d : removed) {
-            System.out.println("triple removed by edition -->: " + d);
-        }
-        System.out.println("-------------------------------------------");
-        System.out.println("Triples added having inverse Prop :");
-        List<Statement> inverses = CommonsValidate.getNeighboursFromInverse(removed);
-        System.out.println("Neighbours Inverse >>" + inverses);
-        System.out.println("-------------------------------------------");
-        System.out.println("Triples added having symetric Prop :");
-        List<Statement> symetrics = CommonsValidate.getNeighboursFromSymmetric(removed);
-        System.out.println("Neighbours Symmetric >>" + symetrics);
-        System.out.println("-------------------------------------------");
-        System.out.println("Candidates triples for removing :");
-        HashMap<String, List<Triple>> toDelete = CommonsValidate.getTriplesToRemove(symetrics, inverses);
-        for (String key : toDelete.keySet()) {
-            List<Triple> tp = toDelete.get(key);
-            for (Triple t : tp) {
-                System.out.println("Triple to Delete >> " + t);
-            }
-        }
-        assertTrue(toDelete.get(EditConstants.BDG + "P6609") != null);
-        assertTrue(toDelete.get(EditConstants.BDG + "P6609").size() == 3);
     }
 
 }

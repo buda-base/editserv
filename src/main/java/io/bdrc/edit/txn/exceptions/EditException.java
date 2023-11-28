fc282d636e77d3d2ed5535136ff825de73d461da
eroux <elie.roux@telecom-bretagne.eu>
2023-11-28 12:13:08 +0100

    use ResponseStatusException

--- src/main/java/io/bdrc/edit/txn/exceptions/EditException.java
+++ src/main/java/io/bdrc/edit/txn/exceptions/EditException.java
@@ -3,29 +3,25 @@ package io.bdrc.edit.txn.exceptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-public class EditException extends Exception {
+public class EditException extends ResponseStatusException {
 
-    int status;
+    int status = 500;
 	private static final long serialVersionUID = 7440728387322903030L;
 	private static final Logger logger = LoggerFactory.getLogger(EditException.class);
 
-	public EditException() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
 	public EditException(String message, Throwable cause, boolean enableSuppression,
 			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
+		super(500, message, cause);
 	}
 
 	public EditException(String message, Throwable cause) {
-		super(message, cause);
+		super(500, message, cause);
 	}
 
     public EditException(int status, String message, Throwable cause) {
-        super(message, cause);
+        super(status, message, cause);
         this.status = status;
         if (status == 500) {
             logger.error("error status {}, message: {}", status, message);
@@ -33,7 +29,7 @@ public class EditException extends Exception {
     }
 	
     public EditException(int status, String message) {
-        super(message);
+        super(status, message, null);
         this.status = status;
         if (status == 500) {
             logger.error("error status {}, message: {}", status, message);
@@ -41,11 +37,11 @@ public class EditException extends Exception {
     }
     
 	public EditException(String message) {
-		super(message);
+		super(500, message, null);
 	}
 
 	public EditException(Throwable cause) {
-		super(cause);
+		super(500, "", cause);
 	}
 	
     public HttpStatus getHttpStatus() {
